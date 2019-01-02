9b99c5116da88e14d624be4abfaa723dd36ec9e6
Marco <sciortino.mrc@gmail.com>
2019-01-02 09:45:48 +0000

    require filestream

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser= require ('body-parser');
  const cors= require('cors');
+ const fs=require('fs');
 const knex=require('knex')({
   client: 'pg',
   connection: {
